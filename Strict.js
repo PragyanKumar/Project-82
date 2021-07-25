f0b3a37ce898ee1cc237e29bce7a60526a9375a1
PragyanKumar <81701254+PragyanKumar@users.noreply.github.com>
2021-07-25 21:34:22 +0530

    Add files via upload

--- Strict.js
+++ Strict.js
@@ -1,5 +1,4 @@
 var mouseEvent="empty"
-var lastPosition_ofX,lastPosition_ofY
 canvas=document.getElementById("myCanvas")
 ctx=canvas.getContext("2d")
 color="chocolate"
@@ -9,6 +8,7 @@ function my_mouse_down(e){
     color=document.getElementById("color").value
     widthOfLine=document.getElementById("width_of_line").value
     mouseEvent="mousedown"
+    radius=document.getElementById("radius").value
 }
 canvas.addEventListener("mouseup",my_mouse_up)
 function my_mouse_up(e){
@@ -23,18 +23,16 @@ function my_mouse_move(e){
     currentPosition_of_mouseX=e.clientX - canvas.offsetLeft
     currentPosition_of_mouseY=e.clientY - canvas.offsetTop
 
-    if (mouseEvent == "mouseDown") {
+    if (mouseEvent == "mousedown") {
         console.log("current position of x and y coordinates = ")
         console.log("x = " + currentPosition_of_mouseX + "y = " + currentPosition_of_mouseY)
         
         ctx.beginPath()
         ctx.strokeStyle = color
         ctx.lineWidth=widthOfLine
-        ctx.arc(currentPosition_of_mouseY,currentPosition_of_mouseY,radius,0,2*Math.PI)
+        ctx.arc(currentPosition_of_mouseX,currentPosition_of_mouseY,radius,0,2*Math.PI)
         ctx.stroke()
     }
-    lastPosition_ofX=currentPosition_of_mouseX
-    lastPosition_ofY=currentPosition_of_mouseY
 }
 function clearArea(){
     ctx.clearRect(0,0,canvas.width,canvas.height)
